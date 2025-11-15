e4ff2f18a4b0a8b95971381650421d481bef77a5
ESME_PRATAP <pratapchoubey3@gmail.com>
2025-11-16 04:18:17 +0530

    Update routing.py

--- mainapp/routing.py
+++ mainapp/routing.py
@@ -12,6 +12,8 @@ from .consumers import GroupChatConsumer
 websocket_urlpatterns = [
     re_path(r"ws/chat/", consumers.ChatConsumer.as_asgi()),
     # re_path(r"ws/private/(?P<room_name>[\w\-]+)/", consumers.PrivateChatConsumer.as_asgi()),
-     re_path(r"^ws/private/(?P<room_name>[\w\-]+)/$", consumers.PrivateChatConsumer.as_asgi()),
+     # re_path(r"^ws/private/(?P<room_name>[\w\-]+)/$", consumers.PrivateChatConsumer.as_asgi()),
+    re_path(r"ws/private/(?P<room_name>[\w\-]+)/", consumers.PrivateChatConsumer.as_asgi()),
+
       re_path(r"ws/group/(?P<group_id>\w+)/$", GroupChatConsumer.as_asgi()),
 ]
