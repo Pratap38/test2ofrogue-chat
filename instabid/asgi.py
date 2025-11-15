7fded1b05760c2084746c77f254f8b7e95cf6a85
ESME_PRATAP <pratapchoubey3@gmail.com>
2025-11-16 04:01:34 +0530

    Update asgi.py

--- instabid/asgi.py
+++ instabid/asgi.py
@@ -4,11 +4,18 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from mainapp.routing import websocket_urlpatterns
 
+# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instabid.settings')
 
+# Standard Django ASGI application
+django_asgi_app = get_asgi_application()
+
+# ProtocolTypeRouter defines how to handle different connection types
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    "http": django_asgi_app,  # HTTP requests
+    "websocket": AuthMiddlewareStack(  # WebSocket requests with user auth
+        URLRouter(
+            websocket_urlpatterns  # Your URL patterns for WS
+        )
     ),
 })
